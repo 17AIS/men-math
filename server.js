f5d2436673d3c840f162be335b67d1502d1539ff
AIS (REAL) <potato@jeffbezos.com>
2025-12-23 16:22:40 +1100

    made the local server work, and then tested placeholder html webpage

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const app = express()
+const PORT = 3000
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static('public'))
+
+// app.get('/', (res) => {
+//     res.sendFile(path.join(__dirname, 'public', ))
+// })
+
+app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}/`)
+})
